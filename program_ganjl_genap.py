a65ff4ee20446ca03a6790d8a55bdcacba72a955
RandS <rafaalhasri2011@gmail.com>
2026-09-10 09:33:51 +0700

    Delete program_ganjl_genap.py

--- program_ganjl_genap.py
+++
@@ -1,9 +0,0 @@
-print ("=======PROGRAM GENAP=======")
-awal = int(input("Ketik nilai awal anda = "))
-selisih =  int(input("Ketik nilai selisihnya = "))
-akhir =  int(input("Ketik nilai akhir anda = "))
-
-while awal <= akhir:
-    if awal % 2 == 0:
-        print (awal)
-    awal += selisih 
